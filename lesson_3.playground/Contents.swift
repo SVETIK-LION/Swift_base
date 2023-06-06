5a5891065f535e84d805df6426232c859a1667f4
svetik_lion <svetlana.lvovskaya@gmail.com>
2023-06-06 20:28:53 +0500

    add homework_5

--- lesson_3.playground/Contents.swift
+++ lesson_3.playground/Contents.swift
@@ -85,36 +85,115 @@ struct PizzaStruct {
 // 3) Создать класс пиццерии, добавить массив с возможными видами пиццы. Переменная с массивом должна быть приватной. Массив задаётся в инициализаторе.
 // 4)Написать в классе пиццерии функции для добавления нового вида пиццы и для получения всех доступных пицц.
 
-class Pizzeria {
-    private var menu: [PizzaStruct]
-    
-    init(menu: [PizzaStruct]) {
-        self.menu = menu
-    }
-    
-    func addPizza(pizza: PizzaStruct) {
-        menu.append(pizza)
-    }
-    
-    func getMenu() -> [PizzaStruct] {
-        return menu
-    }
-}
+//class Pizzeria {
+//    private var menu: [PizzaStruct]
+//
+//    init(menu: [PizzaStruct]) {
+//        self.menu = menu
+//    }
+//
+//    func addPizza(pizza: PizzaStruct) {
+//        menu.append(pizza)
+//    }
+//
+//    func getMenu() -> [PizzaStruct] {
+//        return menu
+//    }
+//}
 
 // 5) Создать экземпляр класса пиццерии и добавить в него несколько видов пицц.
+
+//let margarita = PizzaStruct(price: 100, type: .margarita, dough: "thin", supplements: [Supplements.tomatoes, Supplements.yellowCheese])
+//let fourCheeses = PizzaStruct(price: 250, type: .fourCheeses, dough: "thin", supplements: [Supplements.cheeseWithMold, Supplements.parmesan, Supplements.yellowCheese, Supplements.gaudaCheese])
+//let beef = PizzaStruct(price: 300, type: .beef, dough: "thick", supplements: [Supplements.beef, Supplements.potatoes, Supplements.yellowCheese])
+//let chickenPineapple = PizzaStruct(price: 150, type: .chickenPineapple, dough: "thick", supplements: [Supplements.chicken, Supplements.pineapple, Supplements.yellowCheese])
+//let vegetarian = PizzaStruct(price: 100, type: .vegetarian, dough: "thick", supplements: [Supplements.pepper, Supplements.tomatoes, Supplements.mushrooms])
+//
+//var coolPizzeria = Pizzeria(menu: [beef, fourCheeses])
+//coolPizzeria.addPizza(pizza: fourCheeses)
+//coolPizzeria.addPizza(pizza: margarita)
+//coolPizzeria.addPizza(pizza: chickenPineapple)
+//coolPizzeria.addPizza(pizza: vegetarian)
+//
+//let menu = coolPizzeria.getMenu()
+//print(menu)
+
+
+// Домашняя работа 5
+
+/* 1) В рамках прошлого домашнего задания был создан класс пиццерии с переменной, в которой хранится пицца, удалите её. Необходимо создать структуру картошки фри, в которой будет стоимость и размер картошки и сделать так, чтобы в классе пиццерии была одна переменная, в которую можно было бы класть и пиццу, и картошку фри.
+ 
+ 2) Добавьте в класс пиццерии функцию, которая будет добавлять новую позицию в меню.
+
+3) Создайте протокол, в котором будут содержаться функции открытия и закрытия.
+
+4) Написать расширение для класса пиццерии, в котором будет реализован протокол из пункта 3.
+
+5) Написать функцию, в которой происходит вычитание одного числа из другого. Функция должна работать и с Int, и с Double.Функция должна возвращать результат вычитания. */
+
+print("~~~~~~~~~~~~~~~~~~")
+
+struct FriePotatoesStruct {
+    var price: Double
+    var size: String
+}
+ 
+class Pizzeria {
+     private var menu: [Any]
+     
+     init(menu: [Any]) {
+         self.menu = menu
+     }
+     
+     func addDish(dish: Any) {
+         menu.append(dish)
+     }
+     
+     func getMenu() -> [Any] {
+         return menu
+         
+     func open() {
+         print("OPEN Пиццерия открылась!")
+     }
+     
+     func close() {
+         print("CLOSE Пиццерия закрылась!")
+     }
+         
+     }
+ }
+
+protocol OpenClose {
+    func open()
+    func close()
+}
+
+extension Pizzeria: OpenClose {
+    func open() {
+        print("Мы уже работаем OPEN!")
+    }
+    
+    func close() {
+        print("Мы уже открыты CLOSE!")
+    }
+}
 
 let margarita = PizzaStruct(price: 100, type: .margarita, dough: "thin", supplements: [Supplements.tomatoes, Supplements.yellowCheese])
 let fourCheeses = PizzaStruct(price: 250, type: .fourCheeses, dough: "thin", supplements: [Supplements.cheeseWithMold, Supplements.parmesan, Supplements.yellowCheese, Supplements.gaudaCheese])
 let beef = PizzaStruct(price: 300, type: .beef, dough: "thick", supplements: [Supplements.beef, Supplements.potatoes, Supplements.yellowCheese])
 let chickenPineapple = PizzaStruct(price: 150, type: .chickenPineapple, dough: "thick", supplements: [Supplements.chicken, Supplements.pineapple, Supplements.yellowCheese])
 let vegetarian = PizzaStruct(price: 100, type: .vegetarian, dough: "thick", supplements: [Supplements.pepper, Supplements.tomatoes, Supplements.mushrooms])
+let friesPotatoes = FriePotatoesStruct(price: 40, size: "Small")
 
 var coolPizzeria = Pizzeria(menu: [beef, fourCheeses])
-coolPizzeria.addPizza(pizza: fourCheeses)
-coolPizzeria.addPizza(pizza: margarita)
-coolPizzeria.addPizza(pizza: chickenPineapple)
-coolPizzeria.addPizza(pizza: vegetarian)
+
+coolPizzeria.addDish(dish: fourCheeses)
+coolPizzeria.addDish(dish: margarita)
+coolPizzeria.addDish(dish: chickenPineapple)
+coolPizzeria.addDish(dish: vegetarian)
+coolPizzeria.addDish(dish: friesPotatoes)
+coolPizzeria.open()
+coolPizzeria.close()
 
 let menu = coolPizzeria.getMenu()
 print(menu)
-                        
